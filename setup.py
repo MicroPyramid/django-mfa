ee8731e625f8c49618214b0644ed3f66b980050d
chaitanya <chaitanya@micropyramid.com>
2016-06-28 15:16:48 +0530

    updated setup file

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 
 # allow setup.py to be run from any path
