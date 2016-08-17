a56cb704967e7d7e4cdaa4a789bc90ee954fce29
chaitanya <chaitanya@micropyramid.com>
2016-08-17 21:21:01 +0530

    updated the middleware

--- django_mfa/views.py
+++ django_mfa/views.py
@@ -4,46 +4,57 @@ import base64
 from django.contrib.auth.decorators import login_required
 from django_mfa.models import *
 from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 
 @login_required
 def security_settings(request):
-    return render(request, 'security.html')
+    twofactor_enabled = is_mfa_enabled(request.user)
+    return render(request, 'security.html', {"twofactor_enabled": twofactor_enabled})
 
 
 @login_required
 def configure_mfa(request):
     qr_code = None
+    base_32_secret = None
     if request.method == "POST":
         base_32_secret = base64.b32encode(request.user.email)
         totp_obj = totp.TOTP(base_32_secret)
         qr_code = totp_obj.provisioning_uri(request.user.email)
-        UserOTP.objects.get_or_create(otp_type=request.POST["otp_type"],
-                                      user=request.user,
-                                      secret_key=base_32_secret)
-    return render(request, 'configure.html', {"qr_code": qr_code})
+
+    return render(request, 'configure.html', {"qr_code": qr_code, "secret_key": base_32_secret})
 
 
 @login_required
 def enable_mfa(request):
+    qr_code = None
+    base_32_secret = None
+    is_verified = False
     if request.method == "POST":
-        otp_obj = UserOTP.objects.filter(user=request.user).first()
-        totp_obj = totp.TOTP(otp_obj.secret_key)
+        base_32_secret = request.POST['secret_key']
+        totp_obj = totp.TOTP(request.POST['secret_key'])
         is_verified = totp_obj.verify(request.POST["verification_code"])
         if is_verified:
             request.session['verfied_otp'] = True
+            UserOTP.objects.get_or_create(otp_type=request.POST["otp_type"],
+                                          user=request.user,
+                                          secret_key=request.POST['secret_key'])
+        else:
+            totp_obj = totp.TOTP(base_32_secret)
+            qr_code = totp_obj.provisioning_uri(request.user.email)
 
-    return render(request, 'configure.html', {"is_verified": is_verified})
+    return render(request, 'configure.html', {"is_verified": is_verified, "qr_code": qr_code, "secret_key": base_32_secret})
 
 
 @login_required
 def disable_mfa(request):
     user = request.user
     if not is_mfa_enabled(user):
-        reverse("mfa:configure_mfa")
+        return HttpResponseRedirect(reverse("mfa:configure_mfa"))
     if request.method == "POST":
         user_mfa = user.userotp
         user_mfa.delete()
+        return HttpResponseRedirect(reverse("mfa:configure_mfa"))
     return render(request, 'disable_mfa.html')
 
 
@@ -60,3 +71,4 @@ def verify_otp(request):
         ctx['error_message'] = "Your code is expired or invalid."
     ctx['next_url'] = request.GET["next"]
     return render(request, 'login_verify.html', ctx)
+
