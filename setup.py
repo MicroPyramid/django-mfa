91e2788235a4ab7da9a6a44b3154060163e38914
chaitanya <chaitanya@micropyramid.com>
2016-08-17 21:46:26 +0530

    updated the version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ for dirpath, dirnames, filenames in os.walk(PROJECT_NAME):
 
 setup(
     name='django-mfa',
-    version='0.0.2',
+    version='0.0.3',
     packages=['django_mfa', 'django_mfa.templatetags', 'django_mfa.migrations'],
     include_package_data=True,
     description='A Django deployment package for all hosting types.',
