87250926d43db21d36569f1c53c8e51a9e6e2780
swetha.naretla <swetha.naretla@micropyramid.com>
2016-08-27 19:52:02 +0530

    Added Test cases

--- django_mfa/tests.py
+++ django_mfa/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django_mfa.models import *
+from django.test import Client
+from django_mfa import totp
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+
+class Views_test(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='micro', email='djangomfa@mp.com', password='djangomfa')
+        user_login = self.client.login(username='micro', password="djangomfa")
+
+    def test_security_settings_view(self):
+        response = self.client.get(reverse('security_settings'))
+        self.assertTemplateUsed(response, "security.html")
+
+    def test_configure_mfa_view(self):
+        response = self.client.get(reverse('configure_mfa'))
+        self.assertTemplateUsed(response, "configure.html")
+
+    # def test_configure_mfa_post_view(self):
+    #     response = self.client.post(reverse('configure_mfa'))
+    #     self.assertTemplateUsed(response, "configure.html")
+
+    # def test_enable_mfa_view(self):
+    #     response = self.client.get(reverse('enable_mfa'))
+    #     self.assertTemplateUsed(response, "configure.html")
+
+
--- django_mfa/views.py
+++ django_mfa/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-import totp
+from . import totp
 import base64
 from django.contrib.auth.decorators import login_required
 from django_mfa.models import *
