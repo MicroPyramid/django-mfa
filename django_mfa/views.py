2ff42f743de37232df9dc0b1e97dafd66aaedaea
gregkster <gregkster@gmail.com>
2018-09-20 07:07:54 -0400

    Added remember-my-browser feature.  Does not ask for the code again on the same browser (#31)
    * Make it work with Django 2.0.7
    
    * No need to specify the namespace when including django-mfa urls
    
    * Once MFA is successfuly enabled redirect to LOGIN_REDIRECT_URL
    Pass optional settings.MFA_ISSUER_NAME to the Authenticator app via QR code
    
    * Added remember-my-browser, a feature allowing to use cookie to remember that
    a code has already been entered successfully on a browser and not ask again
    for a defined number of days
    
    * Use redirect to LOGIN_REDIRECT_URL instead of HttpResponseRedirect to support
    both url path and urlpattern name
    
    * Don't set the remember-my-browser cookie when MFA is enabled.  Make the user
    enter the code on next login
    Set cookie secure flag based on DEBUG
    
    * Delete the remember-my-browser cookie when disabling MFA

--- django_mfa/views.py
+++ django_mfa/views.py
@@ -4,10 +4,11 @@ import random
 import re
 
 from django.conf import settings
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django_mfa.models import is_mfa_enabled, UserOTP
 
 from . import totp
@@ -31,7 +32,13 @@ def configure_mfa(request):
         )
         base_32_secret_utf8 = base_32_secret.decode("utf-8")
         totp_obj = totp.TOTP(base_32_secret_utf8)
-        qr_code = re.sub(r'=+$', '', totp_obj.provisioning_uri(request.user.email))
+
+        try:
+            issuer_name = settings.MFA_ISSUER_NAME
+        except:
+            issuer_name = None
+        qr_code = re.sub(r'=+$', '', totp_obj.provisioning_uri(request.user.username, issuer_name=issuer_name))
+
     return render(request, 'django_mfa/configure.html', {"qr_code": qr_code, "secret_key": base_32_secret_utf8})
 
 
@@ -49,11 +56,55 @@ def enable_mfa(request):
             UserOTP.objects.get_or_create(otp_type=request.POST["otp_type"],
                                           user=request.user,
                                           secret_key=request.POST['secret_key'])
+            messages.success(request, "You have successfully enabled multi-factor authentication on your account.")
+            response = redirect(settings.LOGIN_REDIRECT_URL)
+            return response
         else:
             totp_obj = totp.TOTP(base_32_secret)
             qr_code = totp_obj.provisioning_uri(request.user.email)
 
     return render(request, 'django_mfa/configure.html', {"is_verified": is_verified, "qr_code": qr_code, "secret_key": base_32_secret})
+
+
+MFA_COOKIE_PREFIX="RMB_"
+# update Remember-My-Browser cookie
+def update_rmb_cookie(request, response):
+    try:
+        remember_my_browser = settings.MFA_REMEMBER_MY_BROWSER
+        remember_days = settings.MFA_REMEMBER_DAYS
+        cookie_salt = settings.MFA_COOKIE_SALT
+    except:
+        remember_my_browser = False
+    if remember_my_browser:
+        # better not to reveal the username.  Revealing the number seems harmless
+        cookie_name = MFA_COOKIE_PREFIX + str(request.user.pk)
+        response.set_signed_cookie(cookie_name, True, salt=cookie_salt, max_age=remember_days*24*3600,
+                                   secure=(not settings.DEBUG), httponly=True)
+    return response
+
+
+# verify Remember-My-Browser cookie
+# returns True if browser is trusted and no code verification needed
+def verify_rmb_cookie(request):
+    try:
+        remember_my_browser = settings.MFA_REMEMBER_MY_BROWSER
+        max_cookie_age = settings.MFA_REMEMBER_DAYS*24*3600
+        cookie_salt = settings.MFA_COOKIE_SALT
+    except:
+        return False
+    if not remember_my_browser:
+        return False
+    else:
+        cookie_name = MFA_COOKIE_PREFIX + str(request.user.pk)
+        cookie_value = request.get_signed_cookie(cookie_name, False, max_age=max_cookie_age, salt=cookie_salt)
+        # if the cookie value is True and the signature is good than the browser can be trusted
+        return cookie_value
+
+
+def delete_rmb_cookie(request, response):
+    cookie_name = MFA_COOKIE_PREFIX + str(request.user.pk)
+    response.delete_cookie(cookie_name)
+    return response
 
 
 @login_required
@@ -64,7 +115,9 @@ def disable_mfa(request):
     if request.method == "POST":
         user_mfa = user.userotp
         user_mfa.delete()
-        return HttpResponseRedirect(reverse("mfa:configure_mfa"))
+        messages.success(request, "You have successfully disabled multi-factor authentication on your account.")
+        response = redirect(settings.LOGIN_REDIRECT_URL)
+        return delete_rmb_cookie(request, response)
     return render(request, 'django_mfa/disable_mfa.html')
 
 
@@ -88,7 +141,8 @@ def verify_otp(request):
 
             if is_verified:
                 request.session['verfied_otp'] = True
-                return HttpResponseRedirect(request.POST.get("next", settings.LOGIN_REDIRECT_URL))
+                response = redirect(request.POST.get("next", settings.LOGIN_REDIRECT_URL))
+                return update_rmb_cookie(request, response)
             ctx['error_message'] = "Your code is expired or invalid."
 
     ctx['next'] = request.GET.get('next', settings.LOGIN_REDIRECT_URL)
