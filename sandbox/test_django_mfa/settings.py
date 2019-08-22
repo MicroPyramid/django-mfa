48ea9b168c686f309ed13be987dc4b185792da11
pavan <pavan@micropyramid.com>
2019-08-22 16:17:48 +0530

    testcases working with appname

--- sandbox/test_django_mfa/settings.py
+++ sandbox/test_django_mfa/settings.py
@@ -144,4 +144,4 @@ DATABASES = {
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- test_runner.py
+++ test_runner.py
@@ -14,18 +14,26 @@ if __name__ == "__main__":
             }
         },
         INSTALLED_APPS=(
+            'django.contrib.admin',
             'django.contrib.auth',
             'django.contrib.contenttypes',
             'django.contrib.sessions',
+            'django.contrib.messages',
             'django.contrib.staticfiles',
             'django_mfa',
+            'argonauts',
+            'debug_toolbar',
         ),
-        MIDDLEWARE_CLASSES=(
+        MIDDLEWARE=(
+            'django.middleware.security.SecurityMiddleware',
             'django.contrib.sessions.middleware.SessionMiddleware',
             'django.middleware.common.CommonMiddleware',
             'django.middleware.csrf.CsrfViewMiddleware',
             'django.contrib.auth.middleware.AuthenticationMiddleware',
-            'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+            'django.contrib.messages.middleware.MessageMiddleware',
+            'django.middleware.clickjacking.XFrameOptionsMiddleware',
+            'django_mfa.middleware.MfaMiddleware',
+            'debug_toolbar.middleware.DebugToolbarMiddleware',
         ),
         ROOT_URLCONF='django_mfa.urls',
         STATIC_URL='/static/',
