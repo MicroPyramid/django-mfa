13b5f3432fb7e5f40f5f5264a9b9c6c902a40577
Kamil Turek <kamil.turek@hotmail.com>
2021-07-31 18:38:33 +0200

    Fix and clean up sandbox dependencies

--- sandbox/test_django_mfa/settings.py
+++ sandbox/test_django_mfa/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -134,11 +133,11 @@ LOGIN_REDIRECT_URL = "/home/"
 
 ALLOWED_HOSTS = ['*']
 
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
